8b4092fc379d1675ef971f4d81831274af709102
Arath Astorga <xarselt@gmail.com>
2024-08-19 23:26:49 -0600

    feature: add page design, add addNote function

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+const noteContainer = document.getElementById("noteContainer")
 
+const addNoteButton = document.getElementById("addNote")
+
+addNoteButton.addEventListener("click", () => {
+	addNote = noteContainer.insertAdjacentHTML("beforeend", "<article><textarea></textarea><button>delete note</button></article>")
+})
