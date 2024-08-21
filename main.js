d13b9b369e7fcd989aae9bf8ffed4bb52bbf3dc4
Arath Astorga <xarselt@gmail.com>
2024-08-20 22:56:23 -0600

    feature: add remove note function

--- main.js
+++ main.js
@@ -3,5 +3,18 @@ const noteContainer = document.getElementById("noteContainer")
 const addNoteButton = document.getElementById("addNote")
 
 addNoteButton.addEventListener("click", () => {
-	addNote = noteContainer.insertAdjacentHTML("beforeend", "<article><textarea></textarea><button>delete note</button></article>")
+	addNote = noteContainer.insertAdjacentHTML("beforeend", "<article><textarea></textarea><button class='delete-button'>delete note</button></article>")
 })
+
+// removes note on button click
+noteContainer.addEventListener("click", (event) => {
+	if (event.target.classList.contains("delete-button")) {
+		deleteNote(event.target.parentElement)
+	}
+})
+
+function deleteNote(noteItem) {
+	if (confirm("Permanently delete this note?")) {
+		noteItem.remove();
+	}
+}
